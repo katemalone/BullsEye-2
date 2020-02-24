3ed94137b054931fa207dd5eab5a3f6b9c225699
kate.malone <katerosemalone@gmail.com>
2020-02-24 10:42:38 -0700

    Initial Commit

---
+++ BullsEye-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEye-2
+//
+//  Created by Katy Malone on 2/24/20.
+//  Copyright © 2020 Kate Malone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
